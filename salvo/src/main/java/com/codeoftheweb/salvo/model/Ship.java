96a33ed87e1faa4631212a5d9846807c2288c11d
sebastianbogado <sebastian.bogado@kimos.io>
2019-08-29 18:36:07 -0300

    Add ship as entity to fix the problem with other entities

--- salvo/src/main/java/com/codeoftheweb/salvo/model/Ship.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/model/Ship.java
@@ -1,52 +1,44 @@
 package com.codeoftheweb.salvo.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
+@Entity
 public class Ship {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    @ManyToOne
-    private GamePlayer gamePlayerId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@ManyToOne
+	private GamePlayer gamePlayerId;
 
-    //Empty Constructor
-    public Ship() {
-    }
+	//Empty Constructor
+	public Ship() {
+	}
 
-    //Constructor with parameters
-    public Ship(Long id, GamePlayer gamePlayerId) {
-        this.id = id;
-        this.gamePlayerId = gamePlayerId;
-    }
+	//Getters and Setters
 
-    //Getters and Setters
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public GamePlayer getGamePlayerId() {
+		return gamePlayerId;
+	}
 
-    public GamePlayer getGamePlayerId() {
-        return gamePlayerId;
-    }
+	public void setGamePlayerId(GamePlayer gamePlayerId) {
+		this.gamePlayerId = gamePlayerId;
+	}
 
-    public void setGamePlayerId(GamePlayer gamePlayerId) {
-        this.gamePlayerId = gamePlayerId;
-    }
-
-    //to String Method
-    @Override
-    public String toString() {
-        return "Ship{" +
-                "id=" + id +
-                ", gamePlayerId=" + gamePlayerId +
-                '}';
-    }
+	//to String Method
+	@Override
+	public String toString() {
+		return "Ship{" +
+				"id=" + id +
+				", gamePlayerId=" + gamePlayerId +
+				'}';
+	}
 }
--- salvo/src/main/java/com/codeoftheweb/salvo/rest/controller/GamePlayerRestController.java
+++ salvo/src/main/java/com/codeoftheweb/salvo/rest/controller/GamePlayerRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/gamePlayer")
+@RequestMapping("/api/gamePlayer")
 @RestController
 public class GamePlayerRestController {
     @Autowired
